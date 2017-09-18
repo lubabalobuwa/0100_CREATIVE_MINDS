195666d9b4f39f73c9b9fb9cb879aa883910f6c7
sthabile <31071758+sthabile@users.noreply.github.com>
2017-09-18 22:32:48 +0200

    Add files via upload
    Class  Person - all details of the Account Holder

---
+++ Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_slmc
+{
+    class Person
+    {
+
+        public string name { get; set; }
+
+        public string surname { get; set; }
+
+        public int age { get; set; }
+
+        public string id { get; set; }
+
+        public string address { get; set; }
+
+        public string occupation { get; set; }
+
+        public bool female { get; set; }
+
+        public double cellphoneNumber { get; set; }
+
+        
+
+        public Person (string id)
+        {
+            this.id = id;
+
+        }
+
+
+    }
+}
